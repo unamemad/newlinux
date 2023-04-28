e0fcaf0514ba9b0dcd850cc5637d571627b23bd2
Khayrol Islam <92231311+unamemad@users.noreply.github.com>
2023-04-28 06:14:19 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 clear
 chmod +x *
+sleep 1
 mv httpadd /usr/bin
+sleep 1
 mv jonly /usr/bin
+sleep 1
 mv pip /usr/bin
+sleep 1
 mv pip3 /usr/bin
+sleep 1
 mv subfinder /usr/bin
+sleep 1
 mv subr /usr/bin
+echo "Move success.You can Delete this folder "
 
